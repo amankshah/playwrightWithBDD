909267d76a1c7341b492ab5f4eb74574b694d4ed
your-git-username <your-email@example.com>
2024-09-13 17:22:58 +0530

    Tets

---
+++ features/step_definations/steps.js
@@ -0,0 +1,28 @@
+const { when, then, Given } = require("@cucumber/cucumber");
+Given(
+  "a login to Ecommerce application with {username} and {password}",
+  function (string, string2) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When("Add {string} to Cart", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("Verify {string} is displayed in the Cart", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("Enter Valid details and place the order", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("Verify order is present in the orderHistoryPage", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
