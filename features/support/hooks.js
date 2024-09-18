d901713cb97881ed81d605026478e491b1576d4f
your-git-username <your-email@example.com>
2024-09-18 23:48:46 +0530

    After and Before test and steps

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,7 +1,12 @@
-const { Before, After } = require("@cucumber/cucumber");
+const {
+  Before,
+  After,
+  BeforeStep,
+  AfterStep,
+  Status,
+} = require("@cucumber/cucumber");
 const { expect, chromium } = require("@playwright/test");
 const POManager = require("../../tests/pageObjects/POManager");
-
 
 Before(async function () {
   this.browser = await chromium.launch(); // Launch the browser
@@ -11,8 +16,22 @@ Before(async function () {
   this.poManager = new POManager(this.page);
 });
 
-After(async function () {
-  console.log("After hook called");
+After(async function ({ TestInfo, result }) {
+  console.log(" Status: " + result.status);
 
   await this.browser.close();
+});
+
+BeforeStep(async function () {
+  console.log("BeforeStep hook called");
+});
+
+AfterStep(async function ({ result, TestInfo }) {
+  if (result.status === Status.FAILED) {
+    const screenshot = await this.page.screenshot();
+
+    const path = "TestScreenshots.png";
+    await this.page.locator("body").screenshot({ path });
+  }
+  console.log("AfterStep hook called");
 });
