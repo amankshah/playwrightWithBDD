cbdebfd040f6c0bbb019078fc40d6fa11831e94d
your-git-username <your-email@example.com>
2024-09-19 02:15:23 +0530

    to run a 2 senarious parallel feature file  npx cucumber-js features\errorValidation.feature --parallel 2   --exit

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -9,7 +9,8 @@ const { expect, chromium } = require("@playwright/test");
 const POManager = require("../../tests/pageObjects/POManager");
 
 Before(async function () {
-  this.browser = await chromium.launch(); // Launch the browser
+  // this.browser = await chromium.launch(); // Launch the browser
+  this.browser = await chromium.launch({ headless: false }); // Launch the browser headed mode
   this.context = await this.browser.newContext(); // Create a new browser context
   this.page = await this.context.newPage(); // Create a new page
 
